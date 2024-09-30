d818acca357b882613527dbc34e3f4008a3037b6
DawidZielinski23 <zielinski.dawid23@gmail.com>
2024-09-30 09:28:19 +0200

    Update selectors and disabble --headless for xe.com
    Signed-off-by: DawidZielinski23 <zielinski.dawid23@gmail.com>

--- get_instrument_price.py
+++ get_instrument_price.py
@@ -10,19 +10,18 @@ import time
 
 def get_exchange_rate_from_xe(currency, debug):
     url = "https://www.xe.com"
-    css_selector_result = "#__next > div > div:nth-child(4) > div.sc-2b1c5c79-0.frAgUY > section > div:nth-child(2) > div > main > div > div:nth-child(2) > div:nth-child(1) > div > p.sc-e08d6cef-1.fwpLse"
+    css_selector_result = "#__next > div > div:nth-child(4) > div.sc-898f2b1d-0.gRAglo > section > div:nth-child(2) > div > main > div > div:nth-child(2) > div:nth-child(1) > div > p.sc-48043a1-1.hjTGVC"
     css_selector_div_from = "#midmarketFromCurrency"
     css_selector_input_from = "#midmarketFromCurrency > div:nth-child(2) > div > input"
     css_selector_div_to = "#midmarketToCurrency"
     css_selector_input_to = "#midmarketToCurrency > div:nth-child(2) > div > input"
-    css_selector_button_convert = "#__next > div > div:nth-child(4) > div.sc-2b1c5c79-0.frAgUY > section > div:nth-child(2) > div > main > div > div.sc-4f0f6f94-2.chRjcw > button"
-
+    css_selector_button_convert = "#__next > div > div:nth-child(4) > div.sc-898f2b1d-0.gRAglo > section > div:nth-child(2) > div > main > div > div.sc-4f0f6f94-2.chRjcw > button"
     currency = currency.upper()
 
     chrome_options = Options()
     chrome_options.add_argument("--disable-search-engine-choice-screen")
     if debug == False:
-        chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--headless")
         chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
         chrome_options.add_argument('--log-level=3')
     driver = webdriver.Chrome(options = chrome_options)
