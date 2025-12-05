c00c46e404330288f49ec565924bec8177d35eca
DawidZielinski23 <zielinski.dawid23@gmail.com>
2025-12-05 20:12:16 +0100

    Pages' data stored into json Replaced error with raise, try except
    Signed-off-by: DawidZielinski23 <zielinski.dawid23@gmail.com>

--- get_instrument_price.py
+++ get_instrument_price.py
@@ -8,6 +8,7 @@ import re
 import sys
 import time
 import prettytable
+import json
 
 class Driver:
     def __init__(self, url, debug = False, strategy = "normal"):
@@ -38,84 +39,79 @@ class Driver:
             raise RuntimeError("Failed to open url")
 
     def __del__(self):
-        if self.__web is not None:
+        if self.__web != None:
             self.__web.close()
 
     @property
     def web(self):
-        if self.__web is not None:
+        if self.__web != None:
             return self.__web
         else:
             raise RuntimeError("No webdriver created")
 
 
-def get_exchange_rate_from_xe(currency, debug):
-    url = "https://www.xe.com"
-    css_selector_result = "#__next > main > div:nth-child(5) > div > section.relative.w-full.bg-gradient-to-l.from-blue-850.to-blue-700.bg-no-repeat > div.relative.m-auto.box-content.max-w-\[1024px\].px-4.py-8.md\:px-10.md\:pt-16.lg\:pt-20 > div > div > div > div:nth-child(1) > p.text-lg.font-semibold.text-xe-neutral-900.md\:text-2xl > span"
-    css_selector_div_from = "#midmarketFromCurrency"
-    css_selector_input_from = "#midmarketFromCurrency > div:nth-child(2) > div > input"
-    css_selector_div_to = "#midmarketToCurrency"
-    css_selector_input_to = "#midmarketToCurrency > div:nth-child(2) > div > input"
-    css_selector_button_convert = "#__next > main > div:nth-child(5) > section.relative.flex.justify-center.bg-gray-100.pt-8.pb-8.md\:pt-16.md\:pb-16 > div > div > div.flex.flex-col.items-center.gap-6.md\:flex-row.justify-end > button"
+def get_exchange_rate_from_xe(currency, page_json, debug = False, ):
+    if type(currency) != str:
+        raise TypeError("Invalid currency")
+
     currency = currency.upper()
 
-    driver = Driver(url, debug)
+    if type(debug) != bool:
+        raise TypeError("Invalid debug flag")
+
+    if type(page_json) != dict:
+        raise TypeError("Invalid page json")
+
+    driver = Driver(page_json["url"], debug)
 
     try:
-        select_from = driver.web.find_element(By.CSS_SELECTOR, css_selector_div_from)
+        select_from = driver.web.find_element(By.CSS_SELECTOR, page_json["css_selector_div_from"])
         select_from.click()
     except:
-        print("Cannot find div from")
-        return -1
+        raise RuntimeError("Cannot find div from")
 
     try:
-        select_input_from = driver.web.find_element(By.CSS_SELECTOR, css_selector_input_from)
+        select_input_from = driver.web.find_element(By.CSS_SELECTOR, page_json["css_selector_input_from"])
         WebDriverWait(driver.web, 2)
         select_input_from.send_keys(currency)
         time.sleep(1)
         select_input_from.send_keys(Keys.ENTER)
     except:
-        print("Cannot set curency from")
-        return -1
-    
-    try:
-        select_to =  driver.web.find_element(By.CSS_SELECTOR, css_selector_div_to)
-        select_to.click()
-    except:
-        print("Cannot find div to")
-        return -1
+        raise RuntimeError("Cannot set curency from")
 
     try:
-        select_input_to =  driver.web.find_element(By.CSS_SELECTOR, css_selector_input_to)
+        select_to =  driver.web.find_element(By.CSS_SELECTOR, page_json["css_selector_div_to"])
+        select_to.click()
+    except:
+        raise RuntimeError("Cannot find div to")
+
+    try:
+        select_input_to =  driver.web.find_element(By.CSS_SELECTOR, page_json["css_selector_input_to"])
         WebDriverWait(driver.web, 2)
         select_input_to.send_keys("PLN")
         time.sleep(1)
         select_input_to.send_keys(Keys.ENTER)
     except:
-        print("Cannot set currency to")
-        return -1
+        raise RuntimeError("Cannot set currency to")
 
     try:
-        button = driver.web.find_element(By.CSS_SELECTOR, css_selector_button_convert)
+        button = driver.web.find_element(By.CSS_SELECTOR, page_json["css_selector_button_convert"])
         button.click()
         time.sleep(2)
     except:
-        print("Cannot find convert button")
-        return -1
+        raise RuntimeError("Cannot find convert button")
 
     try:
-        WebDriverWait(driver.web, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector_result)))
+        WebDriverWait(driver.web, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, page_json["css_selector_result"])))
     except:
-        print("Exchange value not load")
-        return -1
+        raise RuntimeError("Exchange value not load")
 
     WebDriverWait(driver.web, 2)
 
     try:
-        value_field = driver.web.find_element(By.CSS_SELECTOR, css_selector_result)
+        value_field = driver.web.find_element(By.CSS_SELECTOR, page_json["css_selector_result"])
     except:
-        print("Cannot find exchange value")
-        return -1
+        raise RuntimeError("Cannot find exchange value")
 
     pattern = r"(\d+\.\d+)\s+PLN"
     value_re = re.search(pattern, value_field.text)
@@ -123,20 +119,32 @@ def get_exchange_rate_from_xe(currency, debug):
     try:
         value = float(value_re.group(1))
     except:
-        return -1
+        raise RuntimeError("Cannot convert value")
 
     return value
 
-def get_prize_from_investing(code, instrument, debug):
-    selector = "#__next > div.md\:relative.md\:bg-white > div.relative.flex > div.grid.flex-1.grid-cols-1.px-4.pt-5.font-sans-v2.text-\[\#232526\].antialiased.transition-all.xl\:container.sm\:px-6.md\:gap-6.md\:px-7.md\:pt-10.md2\:gap-8.md2\:px-8.xl\:mx-auto.xl\:gap-10.xl\:px-10.md\:grid-cols-\[1fr_72px\].md2\:grid-cols-\[1fr_420px\] > div.min-w-0 > div.flex.flex-col.gap-6.md\:gap-0 > div.flex.gap-6 > div.flex-1 > div.mb-3.flex.flex-wrap.items-center.gap-x-4.gap-y-2.md\:mb-0\.5.md\:gap-6 > div.text-5xl\/9.font-bold.text-\[\#232526\].md\:text-\[42px\].md\:leading-\[60px\]"
+def get_prize_from_investing(code, instrument, page_json, debug = False):
+    if type(code) != str:
+        raise TypeError("Invalid code")
+
+    if type(instrument) != str:
+        raise TypeError("Invalid instrument")
+
     instrument= instrument.upper()
+
+    if instrument not in {"STOCK", "ETF"}:
+        raise ValueError("Invalid instrument value")
+
+    if type(debug) != bool:
+        raise TypeError("Invalid debug flag")
+
+    if type(page_json) != dict:
+        raise TypeError("Invalid page json")
+
     if instrument == "STOCK":
-        url = "https://pl.investing.com/equities/"
-    elif(instrument == "ETF"):
-        url = "https://pl.investing.com/etfs/"
+        url = page_json["url_stock"]
     else:
-        print("wrong instrument")
-        return -1
+        url = page_json["url_etf"]
 
     complete_url = url + code
 
@@ -145,11 +153,10 @@ def get_prize_from_investing(code, instrument, debug):
     time.sleep(2)
 
     try:
-        prize = driver.web.find_element(By.CSS_SELECTOR, selector)
+        prize = driver.web.find_element(By.CSS_SELECTOR, page_json["selector"])
         split_text = prize.text.split('\n')
     except:
-        print("Cannot get value for string")
-        return -1
+        raise RuntimeError("Cannot get value for string")
 
     value_string = str(split_text[0])
 
@@ -159,25 +166,32 @@ def get_prize_from_investing(code, instrument, debug):
     try:
         value = float(value_string)
     except:
-        print("Cannot convert string value to float")
+        raise RuntimeError("Cannot convert string value to float")
 
     return value
 
-def get_prize_from_trading212(code, debug):
-    code = code.upper()
-    url = "https://www.trading212.com/pl/trading-instruments/invest/"
-    xpath = "//*[@id='__next']/main/section[1]/div/div/div[1]/div[1]/section/div[2]/div/label/label[2]"
+def get_prize_from_trading212(code, page_json, debug = False):
+    if type(code) != str:
+        raise TypeError("Invalid code")
 
-    complete_url = url + code
+    if type(debug) != bool:
+        raise TypeError("Invalid debug flag")
+
+    if type(page_json) != dict:
+        raise TypeError("Invalid page json")
+
+    code = code.upper()
+
+    complete_url = page_json["url"] + code
 
     driver = Driver(complete_url, debug)
 
     try:
-        WebDriverWait(driver.web, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        WebDriverWait(driver.web, 10).until(EC.presence_of_element_located((By.XPATH, page_json["xpath"])))
     except:
-        return -1
+        raise RuntimeError("Prize does not load")
 
-    prize = driver.web.find_element(By.XPATH, xpath)
+    prize = driver.web.find_element(By.XPATH, page_json["xpath"])
     value_string = str(prize.text)
 
     if ',' in value_string:
@@ -186,7 +200,7 @@ def get_prize_from_trading212(code, debug):
     try:
         value = float(value_string)
     except:
-        print("Cannot convert string value to float")
+        raise RuntimeError("Cannot convert string value to float")
 
     return value
 
@@ -196,15 +210,33 @@ if len(sys.argv) > 1:
     found = False
     debug = False
 
-    try:
-        file = open(sys.argv[1], "r")
-    except:
-        print("Failed to open file " + sys.argv[1])
+    with open(sys.argv[1], "r") as input_file:
+        try:
+            lines = input_file.readlines()
+        except:
+            print("Failed to open file " + sys.argv[1])
+
+    with open("xe.json") as xe:
+        try:
+            xe_json = json.load(xe)
+        except:
+            print("Failed to open xe.json")
+
+    with open("investing.json") as investing:
+        try:
+            investing_json = json.load(investing)
+        except:
+            print("Failed to open investing.json")
+
+    with open("trading.json") as trading:
+        try:
+            trading_json = json.load(trading)
+        except:
+            print("Failed to open trading.json")
 
     if len(sys.argv) > 2:
         debug = (sys.argv[2] == "debug")
 
-    lines = file.readlines()
     table = prettytable.PrettyTable(["Element", "Wartość"])
 
     for line in lines:
@@ -230,28 +262,29 @@ if len(sys.argv) > 1:
             currency_value = currency_value_list[index]
             found = False
         else:
-            currency_value = get_exchange_rate_from_xe(currency_code, debug)
-            if currency_value == -1:
+            try:
+                currency_value = get_exchange_rate_from_xe(currency_code, xe_json, debug)
+            except Exception as e:
+                print(e)
                 print("Failed to get exchange rate for " + currency_code)
-                error = 1
+                continue
             else:
                 currency_code_list.append(currency_code)
                 currency_value_list.append(currency_value)
 
         instrument_prize = -1
-        if site == "investing":
-            instrument_prize = get_prize_from_investing(code, instrument, debug)
-        elif site == "trading212":
-            instrument_prize = get_prize_from_trading212(code, debug)
+        try:
+            if site == "investing":
+                instrument_prize = get_prize_from_investing(code, instrument, investing_json, debug)
+            elif site == "trading212":
+                instrument_prize = get_prize_from_trading212(code, trading_json, debug)
+            else:
+                print("Not supported site")
+                continue
+        except Exception as e:
+            print(e)
+            print("Failed to get prize for " + instrument + " " + code)
         else:
-            print("Not supported site")
-            error = 1
-
-        if instrument_prize == -1:
-            print("Failed to get instrument prize for " + instrument + " " + code)
-            error = 1
-
-        if error == 0:
             value = float(count) * float(currency_value) * float(instrument_prize)
             table.add_row([(code + " " + instrument), str(round(value, 2))])
 
